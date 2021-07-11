7db7ea9a22b2eafa1e80f50cdd4ff21ff4c78428
Marko Jovanovic <mjovanovic@infoart.hr>
2021-07-11 13:51:10 +0200

    Add a simple controller

--- src/main/java/com/springframework/sfgdi/SfgDiApplication.java
+++ src/main/java/com/springframework/sfgdi/SfgDiApplication.java
@@ -1,13 +1,26 @@
 package com.springframework.sfgdi;
 
+import com.springframework.sfgdi.controllers.MyController;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class SfgDiApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SfgDiApplication.class, args);
+		ApplicationContext ctx = SpringApplication.run(SfgDiApplication.class, args);
+
+		// bean name is class name starting with a lowercase letter
+		MyController myController = (MyController)ctx.getBean("myController");
+
+		String greeting = myController.sayHello();
+		System.out.println(greeting);
+
+		/* OUTPUT:
+			Hello World!!!
+			Hi Folks!
+		 */
 	}
 
 }
