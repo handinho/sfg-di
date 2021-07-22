6255c9896110fdcb9cfd5b46824703614c9fcd13
Marko Jovanovic <mjovanovic@infoart.hr>
2021-07-22 12:19:55 +0200

    Add tests for controllers and mock DI

---
+++ src/test/java/com/springframework/sfgdi/controllers/SetterInjectedControllerTest.java
@@ -0,0 +1,22 @@
+package com.springframework.sfgdi.controllers;
+
+import com.springframework.sfgdi.services.GreetingServiceImpl;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class SetterInjectedControllerTest {
+
+    SetterInjectedController controller;
+
+    @BeforeEach
+    void setUp() {
+        // mimicking Spring DI
+        controller = new SetterInjectedController();
+        controller.setGreetingService(new GreetingServiceImpl());
+    }
+
+    @Test
+    void getGreeting() {
+        System.out.println(controller.getGreeting());
+    }
+}
